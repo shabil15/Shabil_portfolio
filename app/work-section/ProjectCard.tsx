afce6784370582a73e35de832ec6161e7211373c
SHABIL MOHAMMED <mohammedshabil15@gmail.com>
2025-10-29 20:08:03 +0530

    feat: update ProjectCard layout and adjust project details for improved presentation

--- app/work-section/ProjectCard.tsx
+++ app/work-section/ProjectCard.tsx
@@ -104,7 +104,7 @@ const ProjectCard = ({
       <div
         className={`absolute text-white  ${
           !(id % 2 === 0)
-            ? "right-0 top-32 mr-0 ml-10 md:right-0 md:ml-0 lg:right-0 lg:top-60  lg:mr-4"
+            ? "right-0 top-32 mr-0 ml-12 md:right-0 md:ml-0 lg:right-0 lg:top-60  lg:mr-4"
             : "left-10 top-32 ml-0 md:mr-12 lg:top-52 lg:ml-4"
         } mb-10  md:mb-16 lg:mb-14 `}
       >
@@ -119,7 +119,7 @@ const ProjectCard = ({
         <AnimatedBody
           text={description}
           className={
-            "mt-4 w-[90%] max-w-[457px] text-[16px] font-semibold text-[#95979D] "
+            "mt-4 w-[90%] max-w-[430px] text-[16px] font-semibold text-[#95979D] "
           }
         />
         <div className="mt-9 flex gap-4">
--- app/work-section/projectDetails.ts
+++ app/work-section/projectDetails.ts
@@ -10,116 +10,60 @@ export type ProjectProps = {
 };
 
 export const devProjects = [
-  {
-    id: 0,
-    name: "Odunsi",
-    description:
-      "Portfolio website for Michael Odunsi, an experienced UI/UX designer crafting unique, user-friendly products and web experiences for Web3 founders and projects.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/victorcodess/odunsi-web3-folio",
-    demo: "https://www.odunsi.xyz/",
-    image: require(".//../../public/projects/odunsi.png"),
-    available: true,
-  },
+  // {
+  //   id: 0,
+  //   name: "E-Commerce Platform",
+  //   description:
+  //     "Full-stack e-commerce application built with MERN stack, featuring user authentication, product management, shopping cart, and payment integration with Stripe.",
+  //   technologies: ["React", "Node.js", "MongoDB", "Express", "Redux"],
+  //   github: "https://github.com/mohammedshabil",
+  //   demo: "",
+  //   image: require(".//../../public/projects/odunsi.png"),
+  //   available: true,
+  // },
   {
     id: 1,
-    name: "Interlock",
+    name: "Rentesy",
     description:
-      "This is a website for a Fintech Startup to showcase their innovative solutions tailored to meet the evolving needs of their clients.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/victorcodess/interlock",
-    demo: "https://interlock-teal.vercel.app/",
+      "Architected and built a property management platform for managers, owners, and vendors across India, featuring rent collection, maintenance tracking, financial reporting, tenant portals, and secure payment integration.",
+    technologies: ["Next.js","React", "Node.js", "PostgreSQL"],
+    github: "",
+    demo: "https://www.rentesy.com/",
     image: require(".//../../public/projects/interlock.png"),
     available: true,
   },
   {
     id: 2,
-    name: "Synthetix",
+    name: "FitCall",
     description:
-      "Built specifically for an AI startup, this website lets them present cutting-edge AI data processing solutions tailored to their customers' needs.",
-    technologies: ["React", "Next.js", "Prismic CMS"],
-    github: "https://github.com/victorcodess/synthetix",
-    demo: "https://synthetix-iota.vercel.app/",
+      "Built a fitness platform with Clean Architecture, Redux, WebRTC video calls, real-time chat, OAuth, Stripe payments, and optimized performance—improving load time and user retention.",
+    technologies: ["React", "Node.js", "Express", "MongoDB", "TypeScript"],
+    github: "https://github.com/shabil15/backend-fitcall",
+    demo: "https://fitcall-beta.vercel.app/",
     image: require(".//../../public/projects/synthetix-flip.png"),
     available: true,
   },
   {
     id: 3,
-    name: "Propellent",
+    name: "Brand Kicks",
     description:
-      "A website built for a software startup and small business, to showcase their services and mark their online presence.",
-    technologies: ["React", "Tailwind CSS", "Framer Motion"],
-    github: "https://github.com/victorcodess/propellent",
-    demo: "https://propellent.vercel.app/",
+      "Developed an e-commerce shoe platform with secure login, admin dashboard, product management, order tracking, inventory control, Razorpay payments, and Docker deployment on AWS for scalability.",
+    technologies: ["ejs","express.js", "MongoDB", "Node.js"],
+    github: "https://github.com/shabil15/Brand-Kicks",
+    demo: "https://brandkicks-shop.onrender.com/",
     image: require(".//../../public/projects/propellent-new.png"),
     available: true,
   },
-  {
-    id: 4,
-    name: "Flixify",
-    description:
-      "Flixify lets you seamlessly explore movies and TV series, add bookmarks, and search across all pages. It offers user authentication along with a theme switch.",
-    technologies: ["Next.js", "Typescript", "Prisma"],
-    github: "https://github.com/victorcodess/flixify",
-    demo: "https://flixify.victorwilliams.me/",
-    image: require(".//../../public/projects/flixify.png"),
-    available: true,
-  },
   // {
   //   id: 4,
-  //   name: "SkyWatch",
+  //   name: "Weather Forecast App",
   //   description:
-  //     "SkyWatch is a convenient and user-friendly tool that allows you to quickly and easily check the current weather and forecast for the next 2 days in any city.",
-  //   technologies: ["React", "CSS", "Chart.js"],
-  //   github: "https://github.com/victorcodess/weather-forecast-website",
-  //   demo: "https://sky-watch.vercel.app/",
-  //   image: require(".//../../public/projects/skywatch-flip.png"),
-  //   available: true,
-  // },
-  // {
-  //   id: 4,
-  //   name: "Alpaca Image Generator",
-  //   description:
-  //     "An image generator website that allows users to generate, combine, and download images.",
-  //   technologies: ["React", "CSS", "Merge Images"],
-  //   github: "https://github.com/victorcodess/alpaca-image-generator",
-  //   demo: "http://alpaca-image-generator-beta.vercel.app",
-  //   image: require(".//../../public/projects/alpaca-flip.png"),
-  //   available: true,
-  // },
-
-  // {
-  //   id: 5,
-  //   name: "Link Shortener",
-  //   description:
-  //     "A website that reduces the length of your URL using Bit.ly's API",
-  //   technologies: ["JavaScript", "CSS", "Bit.ly's API"],
-  //   github: "https://github.com/victorcodess/url-shortener",
-  //   demo: "https://url-shortener-nine-delta.vercel.app",
-  //   image: require(".//../../public/projects/shortener-new.webp"),
-  //   available: true,
-  // },
-  // {
-  //   id: 6,
-  //   name: "Carpooling Service",
-  //   description:
-  //     "TMTM helps Covenant University students find fellow students who are headed to the same location, so they can share a ride and split the cost.",
-  //   technologies: ["Material UI", "React", "Formik"],
-  //   github: "https://github.com/victorcodess/carpooling-service",
+  //     "A responsive weather application that provides real-time weather data, 5-day forecasts, and location-based weather information using OpenWeather API.",
+  //   technologies: ["React", "REST API", "Chart.js", "CSS"],
+  //   github: "https://github.com/mohammedshabil",
   //   demo: "",
-  //   image: require(".//../../public/projects/carpool-new.webp"),
-  //   available: false,
-  // },
-  // {
-  //   id: 7,
-  //   name: "MLSC.ng",
-  //   description:
-  //     "This is platform for Microsoft Learn Student Ambassadors to shorten links, append their sharing IDs and generate event certificates.",
-  //   technologies: ["Next.js", "Next Auth", "Tailwind CSS"],
-  //   github: "https://github.com/msp-nigeria/mlsc.ng-frontend",
-  //   demo: "",
-  //   image: require(".//../../public/projects/mlsc.png"),
-  //   available: false,
+  //   image: require(".//../../public/projects/flixify.png"),
+  //   available: true,
   // },
 ];
 
